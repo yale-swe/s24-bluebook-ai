0ce0bc1a7e79edd104d90504f759d756aaaa5264
plin <116143761+plin349@users.noreply.github.com>
2024-02-19 23:54:37 -0500

    mongo db vector search

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
-from lib import chat_completion_request
+from lib import chat_completion_request, create_embedding
+import json
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://dbUser:<password>@cluster0.blvdnja.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://dbUser:<PASSWORD HERE>@cluster0.blvdnja.mongodb.net/?retryWrites=true&w=majority"
 
 # connect to the MongoDB cluster
 client = MongoClient(uri)
+db = client['bluebookai']
+collection = db['course-info']
+
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -24,20 +28,70 @@ def chat():
     data = request.get_json()
     if 'message' not in data:
         return jsonify({"error": "Missing 'messages' in request body"}), 400
+    
     user_messages = data['message']
     response = chat_completion_request(messages=user_messages)
     message = response.choices[0].message
-    if message.tool_calls is not None:
-        json_response = message.tool_calls[0].function.arguments
-        
+    print(message)
+    # if message.tool_calls is None:
+    #     return 'success'
+    #     args = json.loads(message.tool_calls[0].function.arguments)
+    #     query_vector = create_embedding(user_messages[-1]['content'])
+    #     database_response = collection.aggregate([
+    #         {
+    #         '$vectorSearch': {
+    #             'index': 'course-rating-index',
+    #             'path': 'embedding',
+    #             'filter': {
+    #                 'rating': {
+    #                     args['operator']: args['rating']
+    #                 }
+    #             },
+    #             'queryVector': query_vector,
+    #             'numCandidates': 5,
+    #             'limit': 5
+    #         }
+    #         }
+    #     ])
+    #     # print(database_response)
+
+    #     top_class = list(database_response)[0]
+    #     json_response = {
+    #         'title': top_class['title'],
+    #         'rating': top_class['rating'],
+    #     }
+    #     return jsonify(json_response)
+
+    # "{\"operator\":\"$gt\",\"rating\":4}"
     
     # ChatCompletionMessage(content=None, role='assistant', function_call=None, tool_calls=[ChatCompletionMessageToolCall(id='call_Ub07GeA6kaC2OZ8b8KlVmtZz', function=Function(arguments='{\n  "subject_code": "CPSC",\n  "rating": 3.5,\n  "comparison_operator_rating": "$gte",\n  "workload": 1,\n  "comparison_operator_workload": "$lte"\n}', name='CourseFilter'), type='function')])
 
+    query_vector = create_embedding(user_messages[-1]['content'])
+
+    print(user_messages[-1])
+    database_response = collection.aggregate([
+            {
+            '$vectorSearch': {
+                'index': 'course-rating-index',
+                'path': 'embedding',
+                'queryVector': query_vector,
+                'numCandidates': 5,
+                'limit': 5
+            }
+            }
+        ])
+
+    classes = list(database_response)
+    # top_class = classes[0]
+    print([c['title'] for c in classes])
+    top_class = classes[0]
     json_response = {
-        'message': [{
-            'role': response.choices[0].message.role,
-            'content': response.choices[0].message.content,
-        }]
+        # 'message': [{
+        #     'role': response.choices[0].message.role,
+        #     'content': response.choices[0].message.content,
+        # }]
+        'title': top_class['title'],
+        # 'rating': top_class['rating'],
     }
 
     return jsonify(json_response)
--- backend/lib.py
+++ backend/lib.py
@@ -1,62 +1,50 @@
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 import os
 from openai import OpenAI
-import json
 
-# Open the JSON file
-with open('course_subjects.json', 'r') as file:
-    # Load the JSON data into a Python list
-    subjects = json.load(file)
-
-tools = [
+_tools = [
     {
         "type": "function",
         "function": {
-        "name": "CourseFilter",
-        "description": "Provide filters for a course based on conditions.", 
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "subject_code": {
-                    "type": "string",
-                    "enum": [str(key) for key in subjects.keys()],
-                    "description": "A code for the subject of instruction",
+            "name": "filter_classes_by_rating",
+            "description": "",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "operator": {
+                        "type": "string",
+                        "enum": ["$gt", "$lt", '$gte', '$lte', '$eq'],
+                        "description": "The operator to use for the filter",
+                    },
+                    "rating": {
+                        "type": "number",
+                        "description": "The rating to filter by",
+                    },
                 },
-                "rating": {
-                    "type": "number",
-                    "description": "The rating (a number with one significant digit) for the class (0 - 4). If a number is not provided, interpret the given opinion to fit the range. A good, or average, class should be 3.5",
-                },
-                "comparison_operator_rating": {
-                    "type": "string",
-                    "enum": ["$lt", "$gt", "$gte", "$lte"],
-                    "description": "A comparison operator for the class rating",
-                },
-                "workload": {
-                    "type": "number",
-                    "description": "The workload (a number with one significant digit) for the class (0 - 4). If a number is not provided, interpret the given opinion to fit the range.",
-                },
-                "comparison_operator_workload": {
-                    "type": "string",
-                    "enum": ["$lt", "$gt", "$gte", "$lte"],
-                    "description": "A comparison operator for the class workload",
-                },
-                "comparison_operator_workload": {
-                    "type": "string",
-                    "enum": ["$lt", "$gt", "$gte", "$lte"],
-                    "description": "A comparison operator for the class workload",
-                },
-                
+                "required": ["operator", "rating"],
             },
-            "required": ["comparison_operator_rating", "rating"],
-        },
         }
-    }
+    },
 ]
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key="<OPENAI_API_KEY>")
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages, tools=tools, tool_choice=None, model='gpt-3.5-turbo'):
+def create_embedding(text, model='text-embedding-3-small'):
+    try:
+        response = client.embeddings.create(
+            input=text,
+            model="text-embedding-3-small"
+        )
+        return response.data[0].embedding
+    except Exception as e:
+        print("Unable to generate embedding")
+        print(f"Exception: {e}")
+        return e
+
+@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+def chat_completion_request(messages, tools=None, tool_choice=None, model='gpt-3.5-turbo'):
     try:
         response = client.chat.completions.create(
             model=model,
@@ -69,3 +57,4 @@ def chat_completion_request(messages, tools=tools, tool_choice=None, model='gpt-
         print("Unable to generate ChatCompletion response")
         print(f"Exception: {e}")
         return e
+    
