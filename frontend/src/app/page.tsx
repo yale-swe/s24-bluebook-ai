3094962345cda5cd8e670d5d2a4e352f7d8bcf7f
Serena <u.tselmeg@gmail.com>
2024-02-19 20:43:34 -0500

    imessage ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,22 +5,30 @@ import styles from '/Users/tselmegulammandakh/Downloads/cpsc439/s24-bluebook-ai/
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  // initializing with welcome message manually
+  const initialMessage = { id: 'welcome-msg', content: 'How may I help you?', role: 'ai' };
+
   return (
     <div className={styles.chatContainer}>
       <div className={styles.messages}>
-      {messages.map(m => (
-          <div key={m.id} className={`${styles.message} ${m.role === 'user' ? styles.user : ''}`}>
+        <div className={styles.message + ' ' + styles.ai}>{initialMessage.content}</div>
+
+        {messages.map((m) => (
+          <div key={m.id} className={`${styles.message} ${m.role === 'user' ? styles.user : styles.ai}`}>
             {m.content}
           </div>
         ))}
       </div>
       <form onSubmit={handleSubmit} className={styles.inputForm}>
         <input
+          type="text"
           className={styles.inputField}
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
         />
+        <button type="submit" className={styles.sendButton}>Send</button>
       </form>
     </div>
   );
