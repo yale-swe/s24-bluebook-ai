dc17f995a1f2dc44ec0c7c28f3b252f92639311e
Paul Lin <plin@Pauls-MacBook-Pro.local>
2024-03-28 00:03:19 -0400

    add retrieval augmented generation

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,17 @@ import os
 from dotenv import load_dotenv
 from lib import chat_completion_request, create_embedding
 import json
-
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://bluebookairoot:<password>@bluebookcluster.0hf4pzi.mongodb.net/?retryWrites=true&w=majority&appName=BluebookCluster"
+COURSE_QUERY_LIMIT = 5
 
-# connect to the MongoDB cluster
+load_dotenv()
+
+# database initialization
+uri = os.getenv('MONGO_URI')
 client = MongoClient(uri)
-db = client['bluebookai']
-collection = db['course-info']
+db = client['course_db']
+collection = db['parsed_courses']
 
 try:
     client.admin.command('ping')
@@ -19,82 +21,62 @@ try:
 except Exception as e:
     print(e)
 
+# flask
 app = Flask(__name__)
 
-load_dotenv()
-
-@app.route('/chat', methods=['POST'])
+@app.route('/api/chat', methods=['POST'])
 def chat():
-    data = request.get_json()
-    if 'message' not in data:
-        return jsonify({"error": "Missing 'messages' in request body"}), 400
-    
+
+    data = request.get_json()    
     user_messages = data['message']
-    response = chat_completion_request(messages=user_messages)
-    message = response.choices[0].message
-    print(message)
-    # if message.tool_calls is None:
-    #     return 'success'
-    #     args = json.loads(message.tool_calls[0].function.arguments)
-    #     query_vector = create_embedding(user_messages[-1]['content'])
-    #     database_response = collection.aggregate([
-    #         {
-    #         '$vectorSearch': {
-    #             'index': 'course-rating-index',
-    #             'path': 'embedding',
-    #             'filter': {
-    #                 'rating': {
-    #                     args['operator']: args['rating']
-    #                 }
-    #             },
-    #             'queryVector': query_vector,
-    #             'numCandidates': 5,
-    #             'limit': 5
-    #         }
-    #         }
-    #     ])
-    #     # print(database_response)
-
-    #     top_class = list(database_response)[0]
-    #     json_response = {
-    #         'title': top_class['title'],
-    #         'rating': top_class['rating'],
-    #     }
-    #     return jsonify(json_response)
-
-    # "{\"operator\":\"$gt\",\"rating\":4}"
-    
-    # ChatCompletionMessage(content=None, role='assistant', function_call=None, tool_calls=[ChatCompletionMessageToolCall(id='call_Ub07GeA6kaC2OZ8b8KlVmtZz', function=Function(arguments='{\n  "subject_code": "CPSC",\n  "rating": 3.5,\n  "comparison_operator_rating": "$gte",\n  "workload": 1,\n  "comparison_operator_workload": "$lte"\n}', name='CourseFilter'), type='function')])
-
     query_vector = create_embedding(user_messages[-1]['content'])
 
-    print(user_messages[-1])
     database_response = collection.aggregate([
-            {
+        {
             '$vectorSearch': {
-                'index': 'course-rating-index',
+                'index': 'parsed_courses_title_description_index',
                 'path': 'embedding',
+                # 'filter': {
+                #     'rating': {
+                #         args['operator']: args['rating']
+                #     }
+                # },
                 'queryVector': query_vector,
-                'numCandidates': 5,
-                'limit': 5
+                'numCandidates': 30,
+                'limit': COURSE_QUERY_LIMIT
             }
-            }
-        ])
+        }
+    ])
 
-    classes = list(database_response)
-    # top_class = classes[0]
-    print([c['title'] for c in classes])
-    top_class = classes[0]
+    database_response = list(database_response)
+
+    recommended_courses = [
+        {
+            'course_code': course['course_code'],
+            'title': course['title'],
+            'description': course['description']
+        } for course in database_response
+    ]
+    
+    recommendation_prompt = f'Here are some courses that might be relevant to the user request:\n\n'
+    for course in recommended_courses:
+        recommendation_prompt += f'{course["course_code"]}: {course["title"]}\n{course["description"]}\n\n'
+    recommendation_prompt += 'Provide a response to the user. Incorporate this information only if it is relevant to the user request.'
+
+    user_messages.append({
+        'role': 'system',
+        'content': recommendation_prompt
+    })
+
+    response = chat_completion_request(messages=user_messages)
+    response = response.choices[0].message.content
+
     json_response = {
-        # 'message': [{
-        #     'role': response.choices[0].message.role,
-        #     'content': response.choices[0].message.content,
-        # }]
-        'title': top_class['title'],
-        # 'rating': top_class['rating'],
+        'response': response,
+        'courses': recommended_courses
     }
 
     return jsonify(json_response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
