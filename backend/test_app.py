6fc1edb27bf18b892fd1b2746656973655958461
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-04-14 18:13:51 -0400

    troubleshoot github actions 2

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,7 +3,6 @@ from unittest.mock import patch, MagicMock
 from app import create_app
 from flask_testing import TestCase
 from requests_mock import Mocker
-from flask import url_for
 
 
 # Define the TestConfig as a dictionary directly
